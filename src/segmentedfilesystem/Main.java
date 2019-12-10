1896c2038e30bb54d2f4ca81133bc084bd7d60fc
ananyateklu <teklu009@morris.umn.edu>
2019-12-10 14:24:54 -0600

    Revert "create a while loop that adds new files to the map"
    This reverts commit 6996a9523f6d5203a58ad784b720679d79e713c2.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -3,8 +3,6 @@ package segmentedfilesystem;
 
 import java.io.*;
 import java.net.*;
-import java.util.HashMap;
-import java.util.ArrayList;
 
 public class Main {
 
@@ -13,62 +11,17 @@ public class Main {
         InetAddress address;
         DatagramSocket socket = null;
         DatagramPacket packet;
-        byte[] sendBuf = new byte[1028];
+        byte[] sendBuf = new byte[256];
 
         if (args.length != 1) {
             System.out.println("Usage: java Main <hostname>");
             return;
         }
 
-        try {
-            socket = new DatagramSocket();
-        } catch (SocketException e) {
-            System.err.println("Failed to open socket\n" + e);
-        }
-
-        try {
-            address = InetAddress.getByName(args[0]);
-            packet = new DatagramPacket(sendBuf, sendBuf.length, address, port);
-            socket.send(packet);
-        } catch (IOException e) {
-            System.err.println("Failed to send header packet to server\n" + e);
-            return;
-        }
-        ArrayList<FileReceiver> doneFiles = new ArrayList<>();
-        HashMap<Byte, FileReceiver> files = new HashMap<>();
-
-        while (true) {
-            packet = new DatagramPacket(sendBuf, sendBuf.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                System.err.println("Error when receiving packets\n" + e);
-                return;
-            }
-            byte fileId = sendBuf[1];
-            if (files.get(fileId) == null) {
-                files.put(fileId, new FileReceiver(packet));
-            } else {
-                files.get(fileId).addPacket(packet);
-            }
-
-            if (files.get(fileId).isDone()) {
-                doneFiles.add(files.remove(fileId));
-                if (files.isEmpty()) {
-                    break;
-                }
-            }
-
-        }
-
-        for (FileReceiver file : doneFiles) {
-            try {
-                file.createFile();
-            } catch (IOException e) {
-                System.err.println("Error when creating file\n" + e);
-            }
-
-        }
+        socket = new DatagramSocket();
+        address = InetAddress.getByName(args[0]);
+        packet = new DatagramPacket(sendBuf, sendBuf.length, address, port);
+        socket.send(packet);
 
     }
 
