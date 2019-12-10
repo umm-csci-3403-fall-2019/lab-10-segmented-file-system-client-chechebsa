400c09672a25a8b1df04cb47688afaf9d47b98f4
ananyateklu <teklu009@morris.umn.edu>
2019-12-10 13:13:04 -0600

    Open a socket and send an empty packet to start conversation

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,28 @@
+
 package segmentedfilesystem;
 
+import java.io.*;
+import java.net.*;
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        int port = 6014;
+        InetAddress address;
+        DatagramSocket socket = null;
+        DatagramPacket packet;
+        byte[] sendBuf = new byte[256];
+
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <hostname>");
+            return;
+        }
+
+        socket = new DatagramSocket();
+        address = InetAddress.getByName(args[0]);
+        packet = new DatagramPacket(sendBuf, sendBuf.length, address, port);
+        socket.send(packet);
+
     }
 
 }
